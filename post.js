a4b78adf63683bd551fdbcc121dc183effe93d3d
Yarenla Stuardo <104539159+YareStu30@users.noreply.github.com>
2023-01-07 23:29:33 -0300

    fix post.js and add validation there are duplicate fields

--- post.js
+++ post.js
@@ -1,25 +1,48 @@
-equire("dotenv").config();
+require("dotenv").config();
 
-const { Pool } = require("pg");
-
-const pool = new Pool({
-  host: process.env.HOST,
-  user: "postgres",
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
-  allowExitOnIdle: true,
-});
+const pool = require("./helpers/connect").getInstance();
 
 const getPost = async () => {
   const { rows } = await pool.query("SELECT * FROM posts");
   return rows;
 };
-
-const createPost = async (payload) => {
-  const consulta =
-    "INSERT INTO posts (titulo, img, descripcion) VALUES ($1, $2, $3)RETURNING * ";
-  const values = [payload.titulo, payload.url, payload.descripcion];
-  const result = await pool.query(consulta, values);
+const duplicatePost = async (payload) => {
+  const SQLquery = {
+    text: "SELECT COUNT(*) as NUM FROM posts WHERE titulo=$1 AND img=$2 AND descripcion=$3",
+    values: [payload.titulo, payload.url, payload.descripcion],
+  };
+  const { rows } = await pool.query(SQLquery);
+  return rows;
+};
+const addPost = async (payload) => {
+  const resultDuplicate = await duplicatePost(payload);
+  if (resultDuplicate[0].num > 0) {
+    throw { error: " se duplican los campos" };
+  }
+  if (!payload.titulo || !payload.url || !payload.descripcion) {
+    throw { error: "Faltan campos requeridos" };
+  } else {
+    const consulta =
+      "INSERT INTO posts (titulo, img, descripcion, likes) VALUES ($1, $2, $3, $4)RETURNING * ";
+    const values = [payload.titulo, payload.url, payload.descripcion, 0];
+    const result = await pool.query(consulta, values);
+  }
 };
 
-module.exports = { getPost, createPost };
+const addLike = async (id) => {
+  console.log(id);
+  const result = await pool.query(
+    "UPDATE posts SET likes = likes + 1 WHERE id = $1",
+    [id]
+  );
+  console.log(result);
+  return result.rows;
+};
+
+const deletePost = async (id) => {
+  const query = "DELETE FROM posts WHERE id = $1";
+  const values = [id];
+  const result = await pool.query(query, values);
+};
+
+module.exports = { getPost, addPost, deletePost, addLike, duplicatePost };
